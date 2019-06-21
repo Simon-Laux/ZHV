5ffa639f95b5b5c2d37004d73bedddfdf00f0c35
Simon Laux <mobile.info@simonlaux.de>
2019-06-21 22:46:44 +0200

    add some troubleshooting information closes #1

--- app/src/main/java/de/simonlaux/ziphtmlviewer/MainActivity.java
+++ app/src/main/java/de/simonlaux/ziphtmlviewer/MainActivity.java
@@ -1,14 +1,28 @@
 package de.simonlaux.ziphtmlviewer;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    TextView trouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.trouble = findViewById(R.id.troubleshooting);
+        this.trouble.setVisibility(View.INVISIBLE);
     }
+
+    public void toggleTroubleshooting(View view) {
+        if (this.trouble.getVisibility() == View.VISIBLE)
+            this.trouble.setVisibility(View.INVISIBLE);
+        else
+            this.trouble.setVisibility(View.VISIBLE);
+    }
+    
 }
 
