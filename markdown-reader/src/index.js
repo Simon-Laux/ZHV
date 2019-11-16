fce2ea27fec1814c8edddd48c9797994cf0596bd
Simon Laux <mobile.info@simonlaux.de>
2019-11-16 18:20:28 +0100

    add optional test data

--- markdown-reader/src/index.js
+++ markdown-reader/src/index.js
@@ -35,4 +35,52 @@ document.getElementsByTagName('html')[0].classList.value =
 
 const markdown_content = typeof zhv !== "undefined" ? zhv.getMarkdown() : "## ONLY WORKS IN ZHV"
 
+/* Test data, uncoment to use *//*
+const markdown_content = `
+# ZHV - Zipped HTML Viewer
+This is an Android app.
+
+Opens files with the extention \`.html.zip\` or \`.htmlzip\` and displays their content in a webview.
+The app can also open plain html and even markdown files.
+
+It doesn't allow loading resources from the web. Assets must be inline because it only loads from the index html file for now.
+
+## Zipped HTML format
+
+Has extention \`.html.zip\`(for backwards compatibility) or \`.htmlzip\` (that automatic renaming don't break the extention).
+It is an zip file with following content:
+\`\`\`
+example.html.zip
+└── index.html
+\`\`\`
+
+## Special JavaScript
+
+The app exposes the global \`zhv\` object, it can currently be used to check if the site was opened in ZHV and get the version code of ZHV for new functionality that may come in the future.
+
+\`\`\`ts
+zhv : {
+ getVersion:()=>number
+}
+\`\`\`
+
+## Note Regarding Localstorage
+
+If you use local storage please prefix your variables with an unique prefix (your webapp name + random uuid should do it) to avoid collision with other web apps.
+Also don't use \`clear()\` as it clears everything for every webapp.
+
+## Building ZHV
+Debug build:
+
+run \`./gradlew build\` on linux or \`gradlew.bat build\` on windows
+
+[link](#)
+
+table | table | t
+------|-------|---
+t3    | \`t\` | **7&**
+
+`
+*/
+
 document.getElementById("content").innerHTML = marked(markdown_content)
