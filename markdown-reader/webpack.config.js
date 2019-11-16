554fe13d57eb96169c9cf10cb9dedc3e31ffb428
Simon Laux <mobile.info@simonlaux.de>
2019-11-16 04:57:57 +0100

    add babel to use new js in older browsers

--- markdown-reader/webpack.config.js
+++ markdown-reader/webpack.config.js
@@ -19,6 +19,16 @@ module.exports = {
           'css-loader',
           'sass-loader',
         ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
    ]
   },
