8e264f38ba4fccf3c4b605ed1d55ed48999a9bd8
AndroidUserDelta <47091244+RubenDavidPerezJimenez@users.noreply.github.com>
2020-09-14 14:35:04 -0400

    Update OpenActivity.java

--- app/src/main/java/de/simonlaux/ziphtmlviewer/OpenActivity.java
+++ app/src/main/java/de/simonlaux/ziphtmlviewer/OpenActivity.java
@@ -2,11 +2,8 @@ package de.simonlaux.ziphtmlviewer;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -21,31 +18,34 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import de.simonlaux.ziphtmlviewer.R;
+
 public class OpenActivity extends Activity implements View.OnClickListener {
+    private static final String MD_TEXT  = "MD_TEXT";
     private static final String FIRST_TIME_INIT = "FirstTimeInit";
     private static final String IS_SEARCHING = "Searching";
-    private static final String ONE_TIME = "OneTime";
-    private static final String TITLEWEB = "TitleWeb";
+    private static final String TITLE_WEB = "TitleWeb";
     private boolean firstTimeInit = true;
-    private boolean isSearching = true;
-    private boolean onetime = false;
+    private boolean isSearching = false;
+    private JsObject jsObject;
     private String titleWEB ="ZHV";
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (onetime) {
-            Toast.makeText(this, R.string.image_click_explain, Toast.LENGTH_SHORT).show();
-        }
         if (savedInstanceState != null) {
             firstTimeInit = savedInstanceState.getBoolean(FIRST_TIME_INIT, true);
             isSearching = savedInstanceState.getBoolean(IS_SEARCHING, false);
-            onetime = savedInstanceState.getBoolean(ONE_TIME, false);
-            titleWEB =savedInstanceState.getString(TITLEWEB, titleWEB);
+            titleWEB =savedInstanceState.getString(TITLE_WEB, titleWEB);
+            String text=savedInstanceState.getString(MD_TEXT,"");
+            if(text==""){
+                jsObject=new JsObject("",false);
+            }else{
+                jsObject=new JsObject(text,true);
+            }
         }
         /*Don't hide the bar for use it
         try {
@@ -63,7 +63,7 @@ public class OpenActivity extends Activity implements View.OnClickListener {
             this.setTitle(titleWEB);
         }
     }
-    
+
     @SuppressLint("SetJavaScriptEnabled")
     public void configureWebView(WebView web) {
         WebSettings webSettings = web.getSettings();
@@ -71,14 +71,24 @@ public class OpenActivity extends Activity implements View.OnClickListener {
         webSettings.setSupportZoom(true);
         webSettings.setBuiltInZoomControls(true);
         webSettings.setDomStorageEnabled(true);
+        web.addJavascriptInterface(jsObject, "zhv");
+        web.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                OpenActivity.this.titleWEB = title;
+                OpenActivity.this.setTitle(title);
+            }
+        });
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.search_menu, menu);
         final SearchView searchView = (SearchView) menu.findItem(R.id.search_menu_item).getActionView();
         searchView.setIconifiedByDefault(true);
-        searchView.setIconified(true);
+        //searchView.setIconified(true);
         searchView.setImeOptions(EditorInfo.IME_FLAG_NAVIGATE_PREVIOUS | EditorInfo.IME_ACTION_NEXT | EditorInfo.IME_FLAG_NAVIGATE_NEXT);
         searchView.setOnSearchClickListener(this);
         searchView.setSubmitButtonEnabled(true);
@@ -93,10 +103,10 @@ public class OpenActivity extends Activity implements View.OnClickListener {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 WebView myWebView = findViewById(R.id.webview);
-                myWebView.findAllAsync(query);
+                myWebView.findNext(true);
                 return false;
             }
-            
+
             @Override
             public boolean onQueryTextChange(String newText) {
                 WebView myWebView = findViewById(R.id.webview);
@@ -105,9 +115,9 @@ public class OpenActivity extends Activity implements View.OnClickListener {
             }
         });
         return super.onCreateOptionsMenu(menu);
-        
+
     }
-    
+
     @Override
     public void onBackPressed() {
         if (isSearching) {
@@ -119,9 +129,9 @@ public class OpenActivity extends Activity implements View.OnClickListener {
         } else {
             super.onBackPressed();
         }
-        
+
     }
-    
+
     private void handleIntent() {
         Uri uri = getIntent().getData();
         if (uri == null) {
@@ -155,83 +165,33 @@ public class OpenActivity extends Activity implements View.OnClickListener {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         if (text == null) {
             tellUserThatCouldNotOpenFile();
             return;
         }
-        
+
         WebView myWebView = findViewById(R.id.webview);
-        
+        jsObject = new JsObject(text, isMarkdown);
         configureWebView(myWebView);
-        
-        registerForContextMenu(myWebView);
-        
+
         // myWebView.setWebContentsDebuggingEnabled(true);
-        
-        final boolean isMarkdownReader = isMarkdown;
-        final String contentText = text;
-        class JsObject {
-            @JavascriptInterface
-            public int getVersion() {
-                return BuildConfig.VERSION_CODE;
-            }
-            
-            @JavascriptInterface
-            public String toString() {
-                return "[ZippedHTMLViewer Object]";
-            }
-            
-            @JavascriptInterface
-            public String getMarkdown() {
-                if (isMarkdownReader) {
-                    return contentText;
-                } else {
-                    return "";
-                }
-            }
-        }
-        myWebView.addJavascriptInterface(new JsObject(), "zhv");
-        myWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-                OpenActivity.this.titleWEB = title;
-                OpenActivity.this.setTitle(title);
-            }
-        });
+
         myWebView.loadData("", "text/html", null);
-        if (isMarkdown) {
+        if(isMarkdown){
             // markdown mode
             myWebView.loadUrl("file:///android_asset/markdown-reader.html");
         } else {
             // html mode
-            myWebView.loadDataWithBaseURL("file://index.html", contentText, "text/html", null, null);
+            myWebView.loadDataWithBaseURL("file://index.html", text, "text/html", null, null);
         }
         firstTimeInit = false;
     }
-    
-    //on Long touch Not Seen Images send to bot
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        Log.d("TAG", "onCreateContextMenu: " + v.toString());
-        WebView myWebView = findViewById(R.id.webview);
-        WebView.HitTestResult hitTestResult = myWebView.getHitTestResult();
-        if (hitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE) {
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.putExtra(Intent.EXTRA_EMAIL, "");
-            intent.putExtra(Intent.EXTRA_HTML_TEXT, "/web " + hitTestResult.getExtra());
-            intent.putExtra(Intent.EXTRA_TEXT, "/web " + hitTestResult.getExtra());
-            intent.setType("text/html");
-            startActivity(Intent.createChooser(intent, getString(R.string.open_delta)));
-        }
-    }
-    
+
     private void tellUserThatCouldNotOpenFile() {
         Toast.makeText(this, getString(R.string.could_not_open_file), Toast.LENGTH_SHORT).show();
     }
-    
+
     public static String getStringFromZip(InputStream stream) throws IOException {
         ByteArrayOutputStream fout = new ByteArrayOutputStream();
         if (!unpackZip(stream, fout)) {
@@ -239,7 +199,7 @@ public class OpenActivity extends Activity implements View.OnClickListener {
         }
         return fout.toString();
     }
-    
+
     private static boolean unpackZip(InputStream is, ByteArrayOutputStream fout) {
         ZipInputStream zis;
         try {
@@ -250,9 +210,10 @@ public class OpenActivity extends Activity implements View.OnClickListener {
             while ((ze = zis.getNextEntry()) != null) {
                 if (ze.isDirectory())
                     continue;
-                if (!Objects.equals(ze.getName(), "index.html"))
+                //Objects esta del api 19 en adelante
+                if (!ze.getName().contentEquals("index.html"))
                     continue;
-                
+
                 while ((count = zis.read(buffer)) != -1) {
                     fout.write(buffer, 0, count);
                 }
@@ -265,19 +226,43 @@ public class OpenActivity extends Activity implements View.OnClickListener {
         }
         return true;
     }
-    
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         WebView myWebView = findViewById(R.id.webview);
         myWebView.saveState(outState);
         outState.putBoolean(IS_SEARCHING, isSearching);
         outState.putBoolean(FIRST_TIME_INIT, firstTimeInit);
-        outState.putString(TITLEWEB,titleWEB);
+        outState.putString(TITLE_WEB,titleWEB);
+        outState.putString(MD_TEXT,jsObject.getMarkdown());
         super.onSaveInstanceState(outState);
     }
-    
+
     @Override
     public void onClick(View v) {
         isSearching = true;
+    }
+
+
+    class JsObject {
+        String contentText="";
+        boolean isMarkdownReader=false;
+        public JsObject(String contentText,boolean isMarkdownReader){
+            this.contentText=contentText;
+            this.isMarkdownReader=isMarkdownReader;
+        }
+
+        @JavascriptInterface
+        public int getVersion() { return 15; }
+        @JavascriptInterface
+        public String toString() { return "[ZippedHTMLViewer Object]"; }
+        @JavascriptInterface
+        public String getMarkdown() {
+            if(isMarkdownReader){
+                return contentText;
+            } else {
+                return "";
+            }
+        }
     }
 }
